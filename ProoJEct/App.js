a2ea1e83d85dcc0ed769a251faf4331696366fc2
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-12-12 23:44:15 +0500

    Creating APi and fetching data by axios CLI

--- ProoJEct/App.js
+++ ProoJEct/App.js
@@ -1,7 +1,7 @@
-/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import axios from 'axios';
 const App = () => {
   const [data2, setdata2] = useState('');
@@ -16,7 +16,7 @@ const App = () => {
     <View>
       <FlatList
         data={data2}
-        renderItem={item => this.renderItemComponent(item)}
+        renderItem={({item}) => <Text>{item.email}</Text>}
       />
     </View>
   );
